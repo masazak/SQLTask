2434021b00ecdc4b14e345239d479533adea4e47
Masa <masahzak98@gmail.com>
2022-05-18 01:31:10 +0300

    insert data into tables

---
+++ insertData.sql
@@ -0,0 +1,55 @@
+insert into Enemy
+values (1,'Masa','strong working hard') ,
+(2,'Maya','strong working hard') ,
+(3,'Menna','strong working hard') ,
+(4,'Marah','strong working hard') ,
+(5,'Lana','strong working hard') 
+
+insert into Author
+values (1,'Hazem') ,
+(2,'Ahmad') ,
+(3,'Montaser') ,
+(4,'Mohammad') ,
+(5,'Omar') 
+
+insert into Companion
+values (1,'Atheer','true') ,
+(2,'Aseel','false') ,
+(3,'Hadeel','true') ,
+(4,'Majed','false') ,
+(5,'Shahd','true') ,
+(6,'Shatha','true'),
+(7,'Jana','true')
+
+insert into Doctor
+values (1,'Hamza',0597316748,'1998-03-12','2011-03-12','2019-03-12') ,
+(2,'Adel',0597316748,'1997-03-12','2012-04-12','2019-03-12') ,
+(3,'Ibrahem',0597316748,'1996-03-12','2017-03-12','2019-03-12'),
+(4,'Omar',0597316748,'1995-03-12','2011-03-12','2019-03-12') ,
+(5,'Saef',0597316748,'1994-03-12','2021-03-12','2019-03-12')
+
+insert into Episode
+values (1,123,10,'action','go', '2011-03-12' ,4,1,' long episode') ,
+(2,478,8,'horror','be carefull','2012-04-12',4,2,'long episode') ,
+(3,159,5,'action','sing', '2021-06-12',3,1,'long episode') ,
+(4,867,4,'horror','jump', '2019-03-12',2,3,'long episode') ,
+(5,989,1,'horror','listen','2017-09-12',5,4,'long episode')
+
+
+insert into EpisodeEnemy
+values (1,1,1) ,
+(2,2,2) ,
+(3,3,5) ,
+(4,1,1) ,
+(5,4,4) 
+
+
+insert into EpisodeCompanion
+values (1,5,4) ,
+(2,4,4) ,
+(3,2,5) ,
+(4,1,1) ,
+(5,3,5), 
+(6,4,5),
+(7,5,2)
+
